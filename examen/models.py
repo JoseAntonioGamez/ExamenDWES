7d72abcad8017a8d60f22196336e6f7c312a34c1
José Antonio Gámez Granado <jose.gamez.granado.al@iespoligonosur.org>
2025-11-03 14:42:27 +0100

    Configuracion previa al examen

---
+++ examen/models.py
@@ -0,0 +1,6 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
---
+++ examen/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import *
+from django.db.models import Avg, Max, Min, Q, Prefetch
+from django.views.defaults import page_not_found
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html') 
+
+
+
+def mi_error_400(request, exception=None):
+    return render(request, 'errores/400.html', None, None, 400)
+
+def mi_error_403(request, exception=None):
+    return render(request, 'errores/403.html', None, None, 403)
+
+def mi_error_404(request, exception=None):
+    return render(request, 'errores/404.html', None, None, 404)
+
+def mi_error_500(request):
+    return render(request, 'errores/500.html', None, None, 500)
