3859bfe1244d0da19327fed9beb44fc51ba97bb8
José Antonio Gámez Granado <jose.gamez.granado.al@iespoligonosur.org>
2025-11-04 11:10:04 +0100

    Ejercicio1

--- examen/models.py
+++ examen/models.py
@@ -4,3 +4,29 @@ from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Estudio(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+class Sede(models.Model):
+    estudio = models.ForeignKey(Estudio, on_delete=models.CASCADE, related_name='sedes')
+    pais = models.CharField(max_length=100)
+
+class Plataforma(models.Model):
+    nombre = models.CharField(max_length=100)
+    fabricante = models.CharField(max_length=100)
+
+class Videojuego(models.Model):
+    titulo = models.CharField(max_length=200)
+    estudio_desarrollo = models.ForeignKey(Estudio, on_delete=models.CASCADE, related_name='videojuegos')
+    ventas_estimadas = models.IntegerField(null=True, blank=True)
+
+class VideojuegoPlataformas(models.Model):
+    videojuego = models.ForeignKey(Videojuego, on_delete=models.CASCADE, related_name='videojuego_plataformas')
+    plataforma = models.ForeignKey(Plataforma, on_delete=models.CASCADE, related_name='plataformas_videojuegos')
+
+class Analisis(models.Model):
+    videojuego = models.ForeignKey(Videojuego, on_delete=models.CASCADE, related_name='analisis')
+    puntuacion = models.IntegerField(default=50)
+    comentario = models.TextField(null=True, blank=True)
+    fecha = models.DateField(null=True, blank=True)
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fgp^q-g5c6j5f@-=ibdft5*3r%-#n65!$9f70&&tz**ovso_rr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', '0.0.0.0']
 INTERNAL_IPS = ["127.0.0.1"]
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbadMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'mysite.urls'
